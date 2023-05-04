55f4e875be8e76d0d18b66e10bbe32560594ab8c
RaviKantSharma12 <132484390+RaviKantSharma12@users.noreply.github.com>
2023-05-04 15:09:17 +0530

    Add files via upload

---
+++ constant.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const Double pi = 3.14;
+            Console.WriteLine(pi);
+
+        }
+    }
+}
